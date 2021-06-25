d3d86091ef4eddb508dee227247b6ad7748fabb0
BeomsooKim <kbsat@naver.com>
2021-06-25 21:40:56 +0900

    first

---
+++ src/main/java/com/carero/CareroServerApplication.java
@@ -0,0 +1,13 @@
+package com.carero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareroServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carero/CareroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareroServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
